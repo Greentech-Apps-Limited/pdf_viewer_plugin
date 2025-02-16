6aefd1bb32cf36854e1905433ba634fd7cdd0ca3
Shahriar Nasim Nafi <shahriarnasim.nafi@gmail.com>
2025-02-16 18:38:43 +0600

    Remove v1 embedding

--- android/src/main/java/dev/britto/pdf_viewer_plugin/PdfViewerPlugin.java
+++ android/src/main/java/dev/britto/pdf_viewer_plugin/PdfViewerPlugin.java
@@ -2,47 +2,26 @@ package dev.britto.pdf_viewer_plugin;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.BinaryMessenger;
+import androidx.annotation.NonNull;
 
 public class PdfViewerPlugin implements FlutterPlugin {
-  /**
-   * Add an instance of this to {@link io.flutter.embedding.engine.plugins.PluginRegistry} to
-   * register it.
-   *
-   * <p>THIS PLUGIN CODE PATH DEPENDS ON A NEWER VERSION OF FLUTTER THAN THE ONE DEFINED IN THE
-   * PUBSPEC.YAML. Text input will fail on some Android devices unless this is used with at least
-   * flutter/flutter@1d4d63ace1f801a022ea9ec737bf8c15395588b9. Use the V1 embedding with  to use this plugin with older Flutter versions.
-   *
-   * <p>Registration should eventually be handled automatically by v2 of the
-   * GeneratedPluginRegistrant. https://github.com/flutter/flutter/issues/42694
-   */
+  private static final String CHANNEL = "pdf_viewer_plugin";
+
   public PdfViewerPlugin() {}
 
-  /**
-   * Registers a plugin implementation that uses the stable {@code io.flutter.plugin.common}
-   * package.
-   *
-   * <p>Calling this automatically initializes the plugin. However plugins initialized this way
-   * won't react to changes in activity or context, unlike CameraPlugin.
-   */
-  @SuppressWarnings("deprecation")
-  public static void registerWith(io.flutter.plugin.common.PluginRegistry.Registrar registrar) {
-    registrar
-            .platformViewRegistry()
-            .registerViewFactory(
-                    "pdf_viewer_plugin",
-                    new PdfViewerFactory(registrar.messenger(), registrar.view()));
-  }
-
   @Override
-  public void onAttachedToEngine(FlutterPluginBinding binding) {
+  public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
     BinaryMessenger messenger = binding.getBinaryMessenger();
     binding
             .getPlatformViewRegistry()
             .registerViewFactory(
-                    "pdf_viewer_plugin", new PdfViewerFactory(messenger, /*containerView=*/ null));
+                    CHANNEL,
+                    new PdfViewerFactory(messenger, /*containerView=*/ null)
+            );
   }
 
   @Override
-  public void onDetachedFromEngine(FlutterPluginBinding binding) {
+  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+    // Clean up resources if needed
   }
 }
